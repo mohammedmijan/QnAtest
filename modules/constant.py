cdce2955a9e8031009435c14a537c33106c08064
mijan <mejan601@gmail.com>
2021-08-22 18:04:51 +0600

    Cross Origin Connection

--- modules/constant.py
+++
@@ -1,7 +0,0 @@
-from urllib.parse import quote_plus
-
-#---------Variables--------#
-SERVER = {"password":quote_plus("WtH6I16H2NUeNSfC"), "name":"Acrylic_Server"}
-SECRET = "UY&^*^&$^R&TY^I&$^*RFU&^$U%TYGIU^E^$ER&IYUHYE^TY&^&RY"
-COOKIE_CODE = "I am Md. Mizanur Rahman Chowdhury. Irtiaz, set the cookie"
-HACKER = {"success":False,"message":"Sorry, you are a hacker."}
