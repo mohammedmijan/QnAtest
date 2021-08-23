a7766afd9884c132a616a4f8610ccd6ad3fb6cdb
mijan <mejan601@gmail.com>
2021-08-23 21:30:35 +0600

    Create a rule for protecting spammers

--- modules/database.py
+++ modules/database.py
@@ -12,25 +12,27 @@ mongo = PyMongo(app)
 
 def question_server(user,question=None):
     if question:
-        json_question = {"user":user, "question":question, "answer":None, "time":time.time()}
-        mongo.db.question.insert_one(json_question)
-        response =  {"success":True}
-    else:
         questions = mongo.db.question.find({"user": user})
         questions = loads(dumps(questions))
         question = [ ]
         times = questions[-1]["time"] - questions[-2]["time"]
         print(times/60)
         if times/60 >= 3:
-            for quest in questions:
-                ques = decode_encode_test(quest["question"])["question"]
-                question.append({
-                "answer":"I do not know.","question":ques})
-            question.reverse()
-            response = {"success":True,"questions":question } 
+            json_question = {"user":user, "question":question, "answer":None, "time":time.time()}
+            mongo.db.question.insert_one(json_question)
+            response =  {"success":True}
         else:
             response = HACKER
-
+    else:
+        questions = mongo.db.question.find({"user": user})
+        questions = loads(dumps(questions))
+        question = [ ]
+        for quest in questions:
+            ques = decode_encode_test(quest["question"])["question"]
+            question.append({
+            "answer":"I do not know.","question":ques})
+        question.reverse()
+        response = {"success":True,"questions":question } 
     return response
 
 
---
+++ test.py
@@ -0,0 +1,2 @@
+from modules import *
+question_server("Null")
