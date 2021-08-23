ed2b161e288756be38c21977bc4fcedd55c2f91b
mijan <mejan601@gmail.com>
2021-08-23 13:52:17 +0600

    Cross Origin Problems

--- modules/__init__.py
+++ modules/__init__.py
@@ -5,10 +5,11 @@ from modules.database import *
 from modules.machine import *
 
 
-from flask_cors import cross_origin
+from flask_cors import cross_origin, CORS
 from jwt import decode
 from flask import Flask, request
 app = Flask(__name__)
+CORS(app=app)
 app.config["SECRET_KEY"] = SECRET
 
 
